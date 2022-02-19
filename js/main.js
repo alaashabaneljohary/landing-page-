ae948ae17f3675e16f12cd2073e98e2785374f94
alaashaban22 <95075380+alaashaban22@users.noreply.github.com>
2022-02-19 14:11:31 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let clickImg1 = document.getElementById("clickImg1");
+let clickImg2 = document.getElementById("clickImg2");
+let clickImg3 = document.getElementById("clickImg3");
+let homePepsi = document.getElementById("homePepsi");
+
+
+
+clickImg1.onclick=()=>{
+homePepsi.style.backgroundColor = "#084594";
+
+}
+
+clickImg2.onclick=()=>{
+  homePepsi.style.backgroundColor = "#4F091D";
+  
+  }
+
+  clickImg3.onclick=()=>{
+    homePepsi.style.backgroundColor = "#222";
+    
+  }
+
+ 
